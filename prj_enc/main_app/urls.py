0461fbdcf10010290630b3eaf034367822bc7503
Youssef <youssef4581@gmail.com>
2022-05-22 16:42:58 +0100

    added admin pages + routing

--- prj_enc/main_app/urls.py
+++ prj_enc/main_app/urls.py
@@ -5,7 +5,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name="accueil"),
+    path('', views.home, name="home"),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('irrigation/', views.irrigation, name='irrigation'),
+    path('demands/', views.demands, name='demands'),
+    path('employee/', views.employee, name='employee'),
+    path('green-spaces/', views.green_spaces, name='green_spaces'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- prj_enc/main_app/views.py
+++ prj_enc/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 import os
 
 # Create your views here.
@@ -6,3 +7,24 @@ import os
 def home(request):
     print(os.listdir("main_app"))
     return render(request,'index.html')
+
+def about(request):    
+    return HttpResponse('<h1>Not yet available</h1>') 
+
+def login(request):    
+    return render(request, 'pages/login.html')
+
+def register(request):    
+    return render(request, 'pages/register.html') 
+
+def employee(request):    
+    return render(request, 'pages/employee.html') 
+
+def irrigation(request):    
+    return render(request, 'pages/irrigation.html') 
+
+def demands(request):    
+    return render(request, 'pages/demandes.html') 
+
+def green_spaces(request):    
+    return render(request, 'pages/green-spaces.html') 
